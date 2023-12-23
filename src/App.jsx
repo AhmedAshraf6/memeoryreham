fd3decd1c6d5be0a130b13911ef557e6fcb31ca6
ahmed <ahmedp76b@gmail.com>
2023-12-23 05:28:06 -0800

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import React, { useEffect } from 'react';
 export default function App() {
   useEffect(() => {}, []);
   return (
-    <iframe
-      src='https://onedrive.live.com/embed?resid=EEE248D072FB80E6%21710&amp;authkey=!AAN6RfZlmQsLHoE&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1'
-      frameborder='0'
-      style={{ width: '100vw', height: '90vh' }}
-    >
-      This is an embedded{' '}
-      <a target='_blank' href='https://office.com'>
-        Microsoft Office
-      </a>{' '}
-      presentation, powered by{' '}
-      <a target='_blank' href='https://office.com/webapps'>
-        Office
-      </a>
-      .
-    </iframe>
+    <>
+      <iframe
+        src='https://onedrive.live.com/embed?resid=EEE248D072FB80E6%21710&amp;authkey=!AAN6RfZlmQsLHoE&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1'
+        frameborder='0'
+        style={{ width: '100vw', height: '90vh' }}
+      >
+        This is an embedded{' '}
+        <a target='_blank' href='https://office.com'>
+          Microsoft Office
+        </a>{' '}
+        presentation, powered by{' '}
+        <a target='_blank' href='https://office.com/webapps'>
+          Office
+        </a>
+        .
+      </iframe>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <a href='./a.ppsx' download={true}>
+          تحميل الملف
+        </a>
+      </div>
+    </>
   );
 }
